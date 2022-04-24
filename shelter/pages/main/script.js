0f14296bd9ff081ea7013e7fba39fcd0ea4c126e
Maryia Shakola <marishokola89@gmail.com>
2022-04-24 19:33:37 +0300

    feat: implement carousel

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,7 +1,15 @@
+import data from '../../pets.json' assert { type: "json" };
+const petsData = data;
 const burgerMenu = document.querySelector('.burger-menu');
 const navContainer = document.querySelector('.nav-container');
 const wrapper = document.querySelector('.wrapper');
 const overlay = document.querySelector('.overlay');
+const cards = document.querySelectorAll('.card');
+const btnPrev = document.querySelector('.prev');
+const btnNext = document.querySelector('.next');
+const itemPrev = document.querySelector('#item-prev');
+const itemNext = document.querySelector('#item-next');
+const slider = document.querySelector('.slider');
 
 function toggleMenu() {
   burgerMenu.classList.toggle('open');
@@ -22,3 +30,87 @@ function closeMenu(e) {
 burgerMenu.addEventListener('click', toggleMenu);
 overlay.addEventListener('click', toggleMenu);
 navContainer.addEventListener('click', closeMenu);
+
+function getRandom(n) {
+  let arr = [];
+  let randomNum;
+  for (let i = 0; i < n; i++) {
+    while (arr.length < n) {
+      randomNum = Math.floor(Math.random() * 8);
+      if (!arr.includes(randomNum)) {
+        arr.push(randomNum);
+      }
+    }
+  }
+  return arr;
+}
+
+const randomArr = getRandom(8);
+let j = 0;
+const createCard = () => {
+  const card = document.createElement('div');
+  card.className = 'pet-card';
+  card.innerHTML = `
+  <img src="${petsData[randomArr[j]].img}" alt="${petsData[randomArr[j]].name}" width="270" height="270">
+  <h4>${petsData[randomArr[j]].name}</h4>
+  <button class="learn-more-button">Learn more</button>
+  `;
+  if (j === petsData.length - 1) {
+    j = 0;
+  } else {
+    j++;
+  }
+  return card;
+}
+
+cards.forEach(el => {
+  const card = createCard();
+  el.append(card);
+});
+
+const moveLeft = () => {
+  slider.classList.add('transition-left');
+  btnPrev.removeEventListener('click', moveLeft);
+  btnNext.removeEventListener('click', moveRight);
+};
+
+const moveRight = () => {
+  slider.classList.add('transition-right');
+  btnPrev.removeEventListener('click', moveLeft);
+  btnNext.removeEventListener('click', moveRight);
+};
+
+btnPrev.addEventListener('click', moveLeft);
+btnNext.addEventListener('click', moveRight);
+
+slider.addEventListener('animationend', (animationEvent) => {
+  let changedItem;
+  if (animationEvent.animationName === 'move-left') {
+    slider.classList.remove('transition-left');
+    changedItem = itemPrev;
+    document.querySelector('#item-active').innerHTML = itemPrev.innerHTML;
+  } else {
+    slider.classList.remove('transition-right');
+    changedItem = itemNext;
+    document.querySelector('#item-active').innerHTML = itemNext.innerHTML;
+  }
+
+  const cardsContainerWidth = document.querySelector('.cards-container').clientWidth;
+  let n;
+  if (cardsContainerWidth === 990) {
+    n = 3;
+  } else if (cardsContainerWidth === 580) {
+    n = 2;
+  } else {
+    n = 1;
+  }
+
+  changedItem.innerHTML = '';
+  for (let i = 0; i < n; i++) {
+    const card = createCard();
+    changedItem.appendChild(card);
+  }
+
+  btnPrev.addEventListener('click', moveLeft);
+  btnNext.addEventListener('click', moveRight);
+})
